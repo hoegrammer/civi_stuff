e5f61abfe20eeba20c814207886b08e7c4952bf6
naomi <naomirosenberguk@gmail.com>
2015-12-20 18:11:55 +0100

    Moved test file, couldn't get it to run where was

--- test_conversion_into_URL.py
+++ test_conversion_into_URL.py
@@ -14,10 +14,8 @@ class TestMethods(unittest.TestCase):
         output = '{"sequential":1,"contact_type":"Individual","first_name":"John",'\
                  '"last_name":"Smith","api.Email.create":{"email":"jsmith@example.org"}' \
                  ',"api.Address.create":{"location_type_id":"Home","street_address":"2 ' \
-                 'foo st","supplemental_address_1":"Bar", "city": "Sheffield"}}'
-        # NB Postcode appears to have been discarded in output (I think that needs to go in)
+                 'foo st","supplemental_address_1":"Bar", "city": "Sheffield", "postal_code": "s3 9dn"}}'
 
-        # assert (csv_line_to_json_URL_part(input) == output)
         self.assertEqual(csv_line_to_json_URL_part(input), output)
 
 
